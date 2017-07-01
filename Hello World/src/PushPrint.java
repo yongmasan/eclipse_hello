c0bcb62b24de4c4a883a411c848b0ff2306d2ee4
yongmasan <park9554@nate.com>
2017-07-01 14:39:35 +0900

    pushprint.java modified on github

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -4,6 +4,6 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add java Class File Push");
+		System.out.println("world with 42");
 	}
-
 }
