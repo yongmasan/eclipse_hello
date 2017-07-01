528e49efad4ceb094651a0cd5c7bc03dcf2c7140
yongmasan <park9554@nate.com>
2017-07-01 14:52:43 +0900

    modified

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -4,10 +4,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add java Class File Push");
-<<<<<<< HEAD
 		System.out.println("World with younmasan");
-=======
 		System.out.println("world with 42");
->>>>>>> refs/remotes/origin/master
 	}
 }
